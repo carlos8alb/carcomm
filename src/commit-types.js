803fc4d90e8d54c7fc1fde783095ba6f89dde769
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-03-13 16:02:18 -0300

    🆕 feat: Add CLI for commits [breaking change]

---
+++ src/commit-types.js
@@ -0,0 +1,32 @@
+export const COMMIT_TYPES = {
+  feat: {
+    emoji: '🆕',
+    description: 'Add new feature',
+    release: true
+  },
+  fix: {
+    emoji: '✔ ',
+    description: 'Submit a fix to a bug',
+    release: true
+  },
+  perf: {
+    emoji: '📈',
+    description: 'Improve performance',
+    release: true
+  },
+  docs: {
+    emoji: '📄',
+    description: 'Add or update documentation',
+    release: false
+  },
+  test: {
+    emoji: '🧪',
+    description: 'Add or update tests',
+    release: false
+  },
+  build: {
+    emoji: '🏗 ',
+    description: 'Add or update build scripts',
+    release: false
+  }
+}
